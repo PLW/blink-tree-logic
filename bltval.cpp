5b64a826f70821642e16ed2b87cbdb33214b5c22
Paul Pedersen <paulpedersen@localhost.localdomain>
2014-08-27 21:24:14 -0700

    adding bltval.cpp

---
+++ bltval.cpp
@@ -0,0 +1,24 @@
+
+#include "bltval.h"
+
+namespace mongo {
+
+	void BLTVal::BLTVal::putid( uchar* dest, uid id ) {
+	    int i = BtId;
+	    while( i-- ) {
+	        dest[i] = (uchar)id;
+	        id >>= 8;
+	    }
+	}
+	
+	uid BLTVal::BLTVal::getid( uchar* src ) {
+	    uid id = 0;
+	    for (int i = 0; i < BtId; i++) {
+	        id <<= 8;
+	        id |= *src++; 
+	    }
+	    return id;
+	}
+	
+
+}   // namespace mongo
